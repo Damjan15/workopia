69c4e94cf31a0523c355ecd9091b22695d00d742
Damjan15 <stamenovdamjan5@gmail.com>
2025-05-10 19:08:22 +0200

    setup: initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      width: {
+        500: "500px",
+        600: "600px",
+      },
+    },
+  },
+  plugins: [],
+};
