3e231856fdcd1a9a1dfe562dbd35d02eaac7d9e4
Damjan15 <stamenovdamjan5@gmail.com>
2025-05-12 10:09:05 +0200

    feat: helper functions for loading view and partials created, updated files to use helper functions

--- helpers.php
+++ helpers.php
@@ -11,3 +11,27 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @param array $data
+ * @return void
+ */
+function loadView($name)
+{
+    require basePath("views/{$name}.view.php");
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @param array $data
+ * @return void
+ */
+function loadPartial($name)
+{
+    require basePath("views/partials/{$name}.php");
+}
