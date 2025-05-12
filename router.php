782368a5265faecb1dd5b0d608ace10aa9153847
Damjan15 <stamenovdamjan5@gmail.com>
2025-05-12 20:25:50 +0200

    feat: updated router & routes

--- router.php
+++ router.php
@@ -1,11 +1,103 @@
 <?php
 
+class Router
+{
+    protected $routes = [];
 
-$routes = require('routes.php');
+    /**
+     * Register route to the router
+     * 
+     * @param string $method
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function registerRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+        ];
+    }
 
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    http_response_code(404);
-    require $routes['404'];
+
+    /**
+     * Add a GET route to the router
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function get($uri, $controller)
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    /**
+     * Add a POST route to the router
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function post($uri, $controller)
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    /**
+     * Add a PUT route to the router
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function put($uri, $controller)
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    /**
+     * Add a DELETE route to the router
+     * 
+     * @param string $uri
+     * @param string $controller
+     * @return void
+     */
+    public function delete($uri, $controller)
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    /**
+     * Load error page
+     * 
+     * @return void
+     */
+    public function error($httpCode = 404)
+    {
+        http_response_code($httpCode);
+        loadView("error/{$httpCode}");
+        exit;
+    }
+
+    /**
+     * Route the request
+     * 
+     * @param string $uri
+     * @param string $method
+     * @return void
+     */
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === $method) {
+                require basePath($route['controller']);
+                return;
+            }
+        }
+
+        $this->error();
+    }
 }
