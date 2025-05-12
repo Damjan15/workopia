36dd15e23a2c3106aeb543880f3cdfc9a744741e
Damjan15 <stamenovdamjan5@gmail.com>
2025-05-12 10:33:00 +0200

    feat: inspect helper functions created

--- helpers.php
+++ helpers.php
@@ -13,6 +13,33 @@ function basePath($path = '')
 }
 
 /**
+ * Inspect a value
+ * 
+ * @param array $values
+ * @return void
+ */
+function inspect($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a value and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value)
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+
+/**
  * Load a view
  * 
  * @param string $name
