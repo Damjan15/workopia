792463117c27630438d1b48827a3451671ca8226
Damjan15 <stamenovdamjan5@gmail.com>
2025-05-12 10:22:37 +0200

    feat: updated helper functions and added extra checks to make sure the path exists

--- helpers.php
+++ helpers.php
@@ -21,7 +21,14 @@ function basePath($path = '')
  */
 function loadView($name)
 {
-    require basePath("views/{$name}.view.php");
+    $viewPath = basePath("views/{$name}.view.php");
+
+    // Make sure path exists
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View: '{$name}' not found.";
+    }
 }
 
 /**
@@ -33,5 +40,12 @@ function loadView($name)
  */
 function loadPartial($name)
 {
-    require basePath("views/partials/{$name}.php");
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    // Make sure path exists
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial '{$name}' not found. ";
+    }
 }
